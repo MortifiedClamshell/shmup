4056af2d91a5b775d8afb38425547539015dd021
MortifiedClamshell <hailie.m.williams@gmail.com>
2024-09-06 10:27:20 -0500

    Initial commit

---
+++ Scenes/background.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	translate(Vector2.LEFT * 30 * delta)
+
+
+func _on_end_of_level_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print("hello")
+		get_tree().change_scene_to_file("res://world_2.tscn")
---
+++ end_of_level_2.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _process(delta: float) -> void:
+	translate(Vector2.LEFT * 200 * delta)
